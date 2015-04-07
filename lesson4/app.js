59bcf4c56feac1884db2fba6cef7327d3c9b9bfa
wangxu <1228845897@qq.com>
2015-04-07 21:35:12 +0800

    complete get the topic's title,href and first commit

--- lesson4/app.js
+++ lesson4/app.js
@@ -4,6 +4,10 @@ var eventproxy = require('eventproxy');
 var url = require('url');
 var cnodeUrl = "http://cnodejs.org";
 
+var ep = new eventproxy();
+
+//get all topic urls
+var topicUrls = [];
 superagent.get(cnodeUrl)
     .end(function(err,res){
         if(err){
@@ -20,4 +24,30 @@ superagent.get(cnodeUrl)
             topicUrls.push($href);
         })
         console.log(topicUrls);
-    })
+
+        //excute the final result
+        ep.after('topic_html',topicUrls.length,function(topics){
+            topics = topics.map(function(topicPair){
+                var topicUrl = topicPair[0];
+                var topicHtml = topicPair[1];
+                var $ = cheerio.load(topicHtml);
+
+                return {
+                    title : $('.topic_full_title').text().trim(),
+                    href : topicUrl,
+                    commit1 : $('.reply_content').eq(0).text().trim()
+                };
+            });
+            console.log('final:');
+            console.log('topics:'+JSON.stringify(topics));
+        });
+        //excete per url and emit the topic_html
+        topicUrls.forEach(function(topicUrl){
+            superagent.get(topicUrl)
+                .end(function(err,res){
+                    console.log('getch the url successfully');
+                    ep.emit('topic_html',[topicUrl, res.text]);
+                });
+        });
+})
+
