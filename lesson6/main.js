efd324e504124ab619ee4861351623ea3d27d7a2
wangxu <1228845897@qq.com>
2015-04-09 11:11:53 +0800

    complete the fibonacci function

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,18 +1,19 @@
 var fibonacci = function(n) {
-    var result = 0;
     if(n===0){
-        result = 0;
+        return 0;
     }
     if(n===1){
-        result = 1;
+        return 1;
     }
-    return fibonacci(n-1)+fibonacci(n-2);
+    if(n>=2) {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
-console.log('fibonacci(10)'+fibonacci(2));
-/*
+
 exports.fibonacci = fibonacci;
 
-if(require.main===module){
-    var n = 2;
+if(require.main === module){
+    var n = Number(process.argv[2]);
     console.log('fibonacci('+n+'):'+fibonacci(n));
-}*/
+}
+
